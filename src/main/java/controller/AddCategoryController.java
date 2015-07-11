f880cbee59a9a506de6feca11701283452feb342
Edouard Durieux <edouard.durieux@gmail.com>
2015-07-11 10:40:38 +0200

    Adding basic controllers for web pages

---
+++ src/main/java/controller/AddCategoryController.java
@@ -0,0 +1,13 @@
+package main.java.controller;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+/**
+ * Created by Durieux on 11/07/2015.
+ */
+@ApplicationScoped
+@Named("addcategory")
+public class AddCategoryController {
+
+}
