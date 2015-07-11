56c4e5248516000bb4f923959c9f8d8228b9c044
Molinié Vincent <vincent.molinie@epita.fr>
2015-07-11 10:52:59 +0200

    Updating database

--- db/db.sql
+++ db/db.sql
@@ -33,19 +33,20 @@ CREATE TABLE IF NOT EXISTS `article` (
   `id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `content` text NOT NULL,
+  `date` date NOT NULL,
   `user_id` int(11) NOT NULL,
-  `date` date NOT NULL
+  `category_id` int(11) NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `title`, `content`, `user_id`, `date`) VALUES
-(1, 'Article A', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas est metus, ultrices eget dignissim in, euismod id neque. Nullam porttitor nibh sed sapien facilisis vestibulum. Curabitur dignissim lectus sit amet neque tristique, ut elementum enim luctus. Sed convallis arcu at leo eleifend sollicitudin. Nunc ipsum augue, maximus quis ligula sed, volutpat facilisis sapien. Pellentesque urna ligula, maximus quis ornare vel, pellentesque in nisi. Cras congue pharetra blandit. Sed fringilla massa nec sodales elementum. Sed est sapien, vestibulum at tincidunt ut, ultrices non neque. Aliquam facilisis imperdiet gravida. Nam porta massa tempor, aliquam nibh quis, elementum eros. Fusce quis ornare magna. Nunc sed nisl nisi. Nullam et gravida eros. Donec consectetur nisl eu augue ultrices, non imperdiet est tempus. Nam mollis lectus erat, ut semper elit eleifend egestas.', 1, '2015-07-11'),
-(2, 'Article B', 'Nulla dictum ut lorem non tempor. Integer auctor, velit sed molestie egestas, enim nulla blandit massa, sed feugiat mauris metus eu ex. Sed congue lacus vitae dui porta lobortis. Maecenas tristique tristique arcu, in interdum nisl finibus non. Sed dignissim vel lorem id fringilla. Vestibulum eu pulvinar nunc. Morbi accumsan dolor urna, at imperdiet erat luctus et.', 1, '2015-07-20'),
-(3, 'Article Vincent', 'Proin fermentum lacinia dapibus. Aliquam in tempus nibh. Etiam in odio id eros congue posuere. Pellentesque vestibulum mollis massa, ut iaculis eros commodo vitae. Duis lectus est, blandit ac tempus quis, sagittis sed turpis. Sed nec felis id mauris efficitur suscipit nec quis augue. In hac habitasse platea dictumst. Cras id nulla scelerisque arcu pellentesque vestibulum ut ut purus. Donec ac tellus ut ligula vulputate egestas non eu quam.', 2, '2015-07-21'),
-(4, 'Blablabla', 'Suspendisse lacus ipsum, volutpat sit amet varius nec, sodales in ligula. Integer at finibus velit, at porta velit. Maecenas fermentum a diam id hendrerit. Sed commodo erat eget tortor sollicitudin, at lobortis erat porta. Nunc metus nisi, congue vitae dolor vitae, semper vestibulum nisi. Vestibulum ut nibh ut tellus ultrices facilisis. Praesent maximus eros ac ex sagittis, et lobortis ex tincidunt. Sed dignissim finibus metus, vitae mollis eros convallis quis. Donec quis cursus lacus. Cras laoreet dui ac augue placerat maximus. Suspendisse luctus turpis non mauris faucibus, sit amet lacinia dui vulputate. Suspendisse aliquet est quis quam commodo, at gravida mi mollis. Fusce at mauris feugiat, convallis dolor eget, sagittis lacus. Integer nec dapibus sem, nec porttitor sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc ante, lacinia et imperdiet a, cursus in dui.', 1, '2015-07-06');
+INSERT INTO `article` (`id`, `title`, `content`, `user_id`, `date`, `category_id`) VALUES
+(1, 'Article A', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas est metus, ultrices eget dignissim in, euismod id neque. Nullam porttitor nibh sed sapien facilisis vestibulum. Curabitur dignissim lectus sit amet neque tristique, ut elementum enim luctus. Sed convallis arcu at leo eleifend sollicitudin. Nunc ipsum augue, maximus quis ligula sed, volutpat facilisis sapien. Pellentesque urna ligula, maximus quis ornare vel, pellentesque in nisi. Cras congue pharetra blandit. Sed fringilla massa nec sodales elementum. Sed est sapien, vestibulum at tincidunt ut, ultrices non neque. Aliquam facilisis imperdiet gravida. Nam porta massa tempor, aliquam nibh quis, elementum eros. Fusce quis ornare magna. Nunc sed nisl nisi. Nullam et gravida eros. Donec consectetur nisl eu augue ultrices, non imperdiet est tempus. Nam mollis lectus erat, ut semper elit eleifend egestas.', 1, '2015-07-11', 1),
+(2, 'Article B', 'Nulla dictum ut lorem non tempor. Integer auctor, velit sed molestie egestas, enim nulla blandit massa, sed feugiat mauris metus eu ex. Sed congue lacus vitae dui porta lobortis. Maecenas tristique tristique arcu, in interdum nisl finibus non. Sed dignissim vel lorem id fringilla. Vestibulum eu pulvinar nunc. Morbi accumsan dolor urna, at imperdiet erat luctus et.', 1, '2015-07-20', 2),
+(3, 'Article Vincent', 'Proin fermentum lacinia dapibus. Aliquam in tempus nibh. Etiam in odio id eros congue posuere. Pellentesque vestibulum mollis massa, ut iaculis eros commodo vitae. Duis lectus est, blandit ac tempus quis, sagittis sed turpis. Sed nec felis id mauris efficitur suscipit nec quis augue. In hac habitasse platea dictumst. Cras id nulla scelerisque arcu pellentesque vestibulum ut ut purus. Donec ac tellus ut ligula vulputate egestas non eu quam.', 2, '2015-07-21', 1),
+(4, 'Blablabla', 'Suspendisse lacus ipsum, volutpat sit amet varius nec, sodales in ligula. Integer at finibus velit, at porta velit. Maecenas fermentum a diam id hendrerit. Sed commodo erat eget tortor sollicitudin, at lobortis erat porta. Nunc metus nisi, congue vitae dolor vitae, semper vestibulum nisi. Vestibulum ut nibh ut tellus ultrices facilisis. Praesent maximus eros ac ex sagittis, et lobortis ex tincidunt. Sed dignissim finibus metus, vitae mollis eros convallis quis. Donec quis cursus lacus. Cras laoreet dui ac augue placerat maximus. Suspendisse luctus turpis non mauris faucibus, sit amet lacinia dui vulputate. Suspendisse aliquet est quis quam commodo, at gravida mi mollis. Fusce at mauris feugiat, convallis dolor eget, sagittis lacus. Integer nec dapibus sem, nec porttitor sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc ante, lacinia et imperdiet a, cursus in dui.', 1, '2015-07-06', 2);
 
 -- --------------------------------------------------------
 
@@ -56,9 +57,12 @@ INSERT INTO `article` (`id`, `title`, `content`, `user_id`, `date`) VALUES
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `date` date DEFAULT NULL
+  `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Porn'),
+(2, 'Movies');
 
 -- --------------------------------------------------------
 
@@ -112,7 +116,7 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
 -- Indexes for table `article`
 --
 ALTER TABLE `article`
-  ADD PRIMARY KEY (`id`), ADD KEY `fk_article_user_id_idx` (`user_id`);
+  ADD PRIMARY KEY (`id`), ADD KEY `fk_article_user_id_idx` (`user_id`), ADD KEY `fk_article_category_id_idx` (`category_id`);
 
 --
 -- Indexes for table `category`
@@ -165,6 +169,8 @@ ALTER TABLE `user`
 --
 ALTER TABLE `article`
 ADD CONSTRAINT `fk_article_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `article`
+ADD CONSTRAINT `fk_article_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `comment`
